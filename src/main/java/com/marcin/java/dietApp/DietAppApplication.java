146d0168ef50abb3ad0ab04681c2a9485f8b9090
marcin22216 <marcin22216@gmail.com>
2019-01-31 22:25:05 +0100

    init git repo

---
+++ src/main/java/com/marcin/java/dietApp/DietAppApplication.java
@@ -0,0 +1,14 @@
+package com.marcin.java.dietApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietAppApplication.class, args);
+	}
+
+}
+
