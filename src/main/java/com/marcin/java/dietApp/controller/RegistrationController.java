9102fbc2a9294d7d6bf343ecf96fe0b0aa020e3c
marcin22216 <marcin22216@gmail.com>
2019-01-31 23:15:47 +0100

    [marcin][registration] add registretion form and basic logic

--- src/main/java/com/marcin/java/dietApp/controller/RegistrationController.java
+++ src/main/java/com/marcin/java/dietApp/controller/RegistrationController.java
@@ -1,15 +1,29 @@
 package com.marcin.java.dietApp.controller;
 
+import com.marcin.java.dietApp.bean.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.jws.soap.SOAPBinding;
 
 @Controller
 public class RegistrationController {
 
     @GetMapping("/registration")
-    public String registration()
+    public String registration(Model model)
     {
+        model.addAttribute("user", new User());
+
         return "registration/registerPage";
+    }
+
+    @PostMapping("/register")
+    public String register(User userToRegister)
+    {
+
+        return "index";
     }
 
 }
