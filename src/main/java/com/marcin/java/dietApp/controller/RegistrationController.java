5d140f6f1878d33fce420a60fc0726cab1565807
marcin22216 <marcin22216@gmail.com>
2019-01-31 22:58:41 +0100

    [marcin][registration] create controller and getMapping to regitration Page

---
+++ src/main/java/com/marcin/java/dietApp/controller/RegistrationController.java
@@ -0,0 +1,15 @@
+package com.marcin.java.dietApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegistrationController {
+
+    @GetMapping("/registration")
+    public String registration()
+    {
+        return "registration/registerPage";
+    }
+
+}
