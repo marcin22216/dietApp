837b5064da3c1f0de2650acc6fd5e07401f4512b
marcin22216 <marcin22216@gmail.com>
2019-01-31 22:47:04 +0100

    [marcin][registration] create simple mapping localhost

---
+++ src/main/java/com/marcin/java/dietApp/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.marcin.java.dietApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index()
+    {
+        return "index";
+    }
+
+}
