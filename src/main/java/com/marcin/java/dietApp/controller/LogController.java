c48526356778b3c53a7ba43402ef1363fca86181
marcin22216 <marcin22216@gmail.com>
2019-02-01 23:04:19 +0100

    [marcin][registration] create new Login button

---
+++ src/main/java/com/marcin/java/dietApp/controller/LogController.java
@@ -0,0 +1,22 @@
+package com.marcin.java.dietApp.controller;
+
+import com.marcin.java.dietApp.comonent.DataBase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LogController {
+
+    @Autowired
+    private DataBase dataBase;
+
+    @GetMapping("/login")
+    public String login(Model model)
+    {
+        model.addAttribute()
+        return "login/loginPage";
+    }
+
+}
