46b1ac1b564da491f3c4913748c1f67bb0595152
marcin22216 <marcin22216@gmail.com>
2019-02-02 12:03:50 +0100

    [marcin][registration] create very simply logic to check login and password to singn in profile

--- src/main/java/com/marcin/java/dietApp/controller/LogController.java
+++ src/main/java/com/marcin/java/dietApp/controller/LogController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LogController {
@@ -17,6 +18,30 @@ public class LogController {
     public String login(Model model)
     {
         model.addAttribute("user", new User());
+        return "login/loginPage";
+    }
+    @PostMapping("/signed")
+    public String signed(User userToLog)
+    {
+        for (int i=0; i <= dataBase.getUserList().size(); i++)
+        {
+            if (dataBase.getUserList().get(i).getLogin().equals(userToLog.getLogin()))
+            {
+                for (int j=0; i<= dataBase.getUserList().size(); j++)
+                {
+                    if (dataBase.getUserList().get(j).getPassword().equals(userToLog.getPassword()))
+                    {
+                        return "login/userMain";
+                    }
+                    else
+                        return "login/signErrorPass";
+                }
+            }
+            else
+            {
+                return "login/signErrorLog";
+            }
+        }
         return "login/loginPage";
     }
 
